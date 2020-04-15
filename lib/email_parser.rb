810b401925bd4088cebbaaf003aa70c41aff2ed1
Victor Plushko <vicplushko33@gmail.com>
2020-04-15 04:17:56 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,11 +7,10 @@ class EmailAddressParser
   attr_accessor :email_addresses
  
  
- def initialize(email_address)
+ def initialize(email_addresses)
    @email_addresses = email_addresses
  end
  
  def parse
-   
    
 end
